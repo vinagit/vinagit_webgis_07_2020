fb57c56f789278c68292abe73acbdda8062675f4
soiqualang <soiqualang.chentreu@gmail.com>
2020-07-23 13:45:43 +0700

    xuly2geojson.php

--- buoi9_php_sql_js/xuly2geojson.php
+++ buoi9_php_sql_js/xuly2geojson.php
@@ -1,35 +1,29 @@
 <?php
 include('../config.php');
 
-$truyvan='SELECT ST_AsGeoJSON(geom) as json from duynghia_quangnam limit 1;';
+$soto=$_GET['soto'];
+$sothua=$_GET['sothua'];
 
+if($soto!='' and $sothua!=''){
+        $truyvan="SELECT diachi,soto,sothua,dientich,tenchusdd,mdsd, ST_AsGeoJSON( ST_Transform(geom,4326))::json as json from duynghia_quangnam WHERE soto = ".$soto." AND sothua = ".$sothua." LIMIT 50";
+        // echo $truyvan;
+}elseif($soto!=''){
+        $truyvan="SELECT diachi,soto,sothua,dientich,tenchusdd,mdsd, ST_AsGeoJSON( ST_Transform(geom,4326))::json as json from duynghia_quangnam WHERE soto = ".$soto." LIMIT 50";
+        // echo $truyvan;
+}elseif($sothua!=''){
+        $truyvan="SELECT diachi,soto,sothua,dientich,tenchusdd,mdsd, ST_AsGeoJSON( ST_Transform(geom,4326))::json as json from duynghia_quangnam WHERE sothua = ".$sothua." LIMIT 50";
+        // echo $truyvan;
+}else{
+        $truyvan='';
+        echo 'Bạn chưa nhập điều kiện!';
+}
 
 if($truyvan!=''){
         $thucthi=pg_query($dbcon,$truyvan);
         while($kq=pg_fetch_assoc($thucthi)){
-                echo $kq['json'];
+                echo '<b>Tên:</b> '.$kq['tenchusdd'].', <b>địa chỉ:</b> '.$kq['diachi'];
+                echo '<br>';
         }
 }
-
-
-
-
-
-
-// Lệnh select
-/* $truyvan='SELECT shbando as soto,shthua as sothua,tenchusdd as ten, diachi
-FROM "duynghia_quangnam"
-LIMIT 10'; */
-
-// $truyvan='SELECT * FROM duynghia_quangnam WHERE shbando = 11 AND shthua = 2 LIMIT 50';
-
-/* $truyvan="SELECT * FROM duynghia_quangnam WHERE diachi ILIKE '%sơn viên%' AND shthua = 2 LIMIT 50";
-
-$thucthi=pg_query($dbcon,$truyvan);
-
-while($kq=pg_fetch_assoc($thucthi)){
-        echo '<b>Tên:</b> '.$kq['tenchusdd'].', <b>địa chỉ:</b> '.$kq['diachi'];
-        echo '<br>';
-} */
 
 ?>
