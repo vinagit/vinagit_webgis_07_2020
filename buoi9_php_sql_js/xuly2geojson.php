a607dc98b96a70a7bbf727c7b3440961d1859f6f
soiqualang <soiqualang.chentreu@gmail.com>
2020-07-23 14:03:31 +0700

    t1

--- buoi9_php_sql_js/xuly2geojson.php
+++ buoi9_php_sql_js/xuly2geojson.php
@@ -19,11 +19,29 @@ if($soto!='' and $sothua!=''){
 }
 
 if($truyvan!=''){
-        $thucthi=pg_query($dbcon,$truyvan);
+	
+		//Thực thi câu truy vấn trả về HTML
+		
+        /* $thucthi=pg_query($dbcon,$truyvan);
         while($kq=pg_fetch_assoc($thucthi)){
                 echo '<b>Tên:</b> '.$kq['tenchusdd'].', <b>địa chỉ:</b> '.$kq['diachi'];
                 echo '<br>';
-        }
+        } */
+		
+		
+		//Thực thi câu truy vấn trả về JSON
+		
+		$thucthi=pg_query($dbcon,$truyvan);
+		/* $i=pg_num_rows($thucthi)-1; */
+		$array=array();
+		//Lay du lieu
+		while($kq=pg_fetch_assoc($thucthi)){
+			array_push($array,$kq); 
+			/* array_push :php */ 
+		}
+
+		$myJSON = json_encode($array);
+		echo $myJSON;
 }
 
 ?>
